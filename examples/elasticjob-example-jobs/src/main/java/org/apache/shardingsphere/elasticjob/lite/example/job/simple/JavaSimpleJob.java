fd1c5dd8dc48f4f9664e6aca7799b7e06ef9b408
kuanghc1 <kuanghc1@lenovo.com>
2021-12-20 16:11:45 +0800

    Add examples

--- examples/elasticjob-example-jobs/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/job/simple/JavaSimpleJob.java
+++ examples/elasticjob-example-jobs/src/main/java/org/apache/shardingsphere/elasticjob/lite/example/job/simple/JavaSimpleJob.java
@@ -7,7 +7,7 @@
  * the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,26 +18,30 @@
 package org.apache.shardingsphere.elasticjob.lite.example.job.simple;
 
 import org.apache.shardingsphere.elasticjob.api.ShardingContext;
-import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 import org.apache.shardingsphere.elasticjob.lite.example.fixture.entity.Foo;
 import org.apache.shardingsphere.elasticjob.lite.example.fixture.repository.FooRepository;
 import org.apache.shardingsphere.elasticjob.lite.example.fixture.repository.FooRepositoryFactory;
+import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 public class JavaSimpleJob implements SimpleJob {
-    
+
     private final FooRepository fooRepository = FooRepositoryFactory.getFooRepository();
-    
+
     @Override
     public void execute(final ShardingContext shardingContext) {
         System.out.printf("Item: %s | Time: %s | Thread: %s | %s%n",
                 shardingContext.getShardingItem(), new SimpleDateFormat("HH:mm:ss").format(new Date()), Thread.currentThread().getId(), "SIMPLE");
-        List<Foo> data = fooRepository.findTodoData(shardingContext.getShardingParameter(), 10);
-        for (Foo each : data) {
-            fooRepository.setCompleted(each.getId());
-        }
+
+//        Exception e = new Exception("this is a log");
+//        e.printStackTrace();
+//        List<Foo> data = fooRepository.findTodoData(shardingContext.getShardingParameter(), 10);
+//
+//        for (Foo each : data) {
+//            fooRepository.setCompleted(each.getId());
+//        }
     }
 }
